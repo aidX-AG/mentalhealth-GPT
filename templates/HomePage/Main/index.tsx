81f0837f25c209d1e83f59280719ce505952201f
Peter Wildhaber <peter.wildhaber@aidx.ch>
2025-07-09 20:31:25 +0200

    feat: updated homepage tagline for mental health specialization and privacy

--- templates/HomePage/Main/index.tsx
+++ templates/HomePage/Main/index.tsx
@@ -14,11 +14,10 @@ const Main = ({}: MainProps) => {
             <div className="grow px-10 py-20 overflow-y-auto scroll-smooth scrollbar-none 2xl:py-12 md:px-4 md:pt-0 md:pb-6">
                 <div className="mb-10 text-center">
                     <div className="h3 leading-[4rem] 2xl:mb-2 2xl:h4">
-                        Unlock the power of AI
+                        mentalhealthGPT
                     </div>
                     <div className="body1 text-n-4 2xl:body1S">
-                        Chat with the smartest AI - Experience the power of AI
-                        with us
+                        Expertise you trust. Privacy you control. Science that empowers.
                     </div>
                 </div>
                 <Menu className="max-w-[30.75rem] mx-auto" items={navigation} />
