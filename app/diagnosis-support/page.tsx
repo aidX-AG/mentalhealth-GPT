8cdd32f052839c84c79250643a2d1c31c4af115b
Peter Wildhaber <peter.wildhaber@aidx.ch>
2025-07-11 17:18:30 +0200

    chore: cleanup old routes and add new page.tsx structure

---
+++ app/diagnosis-support/page.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import DiagnosisSupportPage from "@/templates/DiagnosisSupportPage";
+
+const DiagnosisSupport: NextPage = () => {
+    return <DiagnosisSupportPage />;
+};
+
+export default DiagnosisSupport;
