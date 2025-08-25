d2872be35585970788a31a6a9d6db2d6794bfc64
Peter Wildhaber <peter.wildhaber@aidx.ch>
2025-08-25 15:57:52 +0200

    chore: remove dev pagelist route from production

--- app/pagelist/page.tsx
+++
@@ -1,8 +0,0 @@
-import PageView from "@/templates/PageListPage";
-import { loadMessages, makeT } from "@/lib/i18n-static";
-
-export default function Page() {
-  const messages = loadMessages("en");
-  const t = makeT(messages);
-  return <PageView />;
-}
