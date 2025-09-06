5984944f6f840b3e298160ecb43a936b392dcf79
Peter Wildhaber <peter.wildhaber@aidx.ch>
2025-09-06 12:30:28 +0000

    i18n(pricing): update plans & FAQ; pushed keys to TX

--- mocks/price.tsx
+++ mocks/price.tsx
@@ -3,83 +3,86 @@ const t = getT();
 
 export const price = [{
   id: "0",
-  title: t("Free"),
+  title: t("Starter"),
   popular: false,
   currentPlan: true,
-  description: t("Basic chat functionality"),
-  priceMonth: 0,
-  priceYear: 0,
-  priceDetails: t("Free forever"),
-  details: [t("30 days history"), t("Up to 1000 messages/mo"), t("Limited AI capabilities")]
+  description: t("Start your spezialized AI experience"),
+  priceMonth: 29,
+  priceYear: 290,
+  priceDetails: t("Per month, per user"),
+  details: [
+    t("90 days history"),
+    t("Up to 1000 messages/mo"),
+    t("Security and data protection")]
 }, {
   id: "1",
   title: t("Pro"),
   colorTitle: "#3E90F0",
   popular: false,
   currentPlan: false,
-  description: t("More advanced AI capabilities"),
+  description: t("Advanced mental health AI models"),
   priceMonth: 89,
-  priceYear: 999,
-  priceDetails: t("Per month, per team members"),
-  details: [t("Email support"), t("Unlimited messages"), t("Access to AI capabilities")]
+  priceYear: 990,
+  priceDetails: t("Per month, per user"),
+  details: [t("Email support"), t("Unlimited messages"), t("Access all AI capabilities")]
 }, {
   id: "2",
-  title: t("Enterprise"),
+  title: t("Institution"),
   colorTitle: "#3FDD78",
   popular: true,
   currentPlan: false,
-  description: t("For large team and business"),
-  priceMonth: 399,
-  priceYear: 2399,
-  priceDetails: t("Per month, per team members"), // (Tipp: “teaam” Tippfehler korrigiert)
-  details: [t("Customizable AI models"), t("Advanced team management"), t("Enterprise-level support")]
+  description: t("For teams and clinics"),
+  priceMonth: 390,
+  priceYear: 3990,
+  priceDetails: t("Per month, per team members"),
+  details: [t("Customizable AI models"), t("Advanced team management"), t("Institution-level support")]
 }];
 
 export const featuresPrice = [{
   id: "0",
-  title: t("Access full to AI capabilities"),
+  title: t("Core access to mental health AI"),
   free: true,
   pro: true,
   enterprise: true
 }, {
   id: "1",
-  title: t("Access to documentation"),
+  title: t("Access to  knowledge base"),
   free: true,
   pro: true,
   enterprise: true
 }, {
   id: "2",
-  title: t("Enterprise-level support"),
-  free: false,
+  title: t("transcription & documentation"),
+  free: true,
   pro: true,
   enterprise: true
 }, {
   id: "3",
-  title: t("Enterprise-level support"),
+  title: t("Specialized AI tuned for diagnostics"),
   free: false,
-  pro: false,
+  pro: true,
   enterprise: true
 }, {
   id: "4",
-  title: t("Support"),
+  title: t("Priority support"),
   free: false,
   pro: true,
   enterprise: true
 }, {
   id: "5",
-  title: t("Integration with CRMs"),
+  title: t("Customizable institutional AI models"),
   free: false,
   pro: false,
   enterprise: true
 }, {
   id: "6",
-  title: t("Customizable AI models"),
+  title: t("AI Training on institution-specific data"),
   free: false,
   pro: false,
   enterprise: true
 }, {
   id: "7",
-  title: t("Dedicated account manager"),
+  title: t("Advanced documentation & reporting tools"),
   free: false,
   pro: false,
   enterprise: true
